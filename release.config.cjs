1315b596fe29fe41bcae3e650e798562af6b4006
t18n <me@t18n.com>
2024-01-12 23:05:06 +0100

    fix: GH action for release

--- release.config.cjs
+++ release.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
+    // "@semantic-release/changelog",
     "@semantic-release/git",
     // "@semantic-release/github",
     // "@semantic-release/npm",
