50aa0eaeefc087e6b736f1945a03153151d5c8cb
t18n <me@t18n.com>
2024-01-12 22:47:21 +0100

    fix: update semantic release action

--- release.config.cjs
+++ release.config.cjs
@@ -12,5 +12,20 @@ module.exports = {
       prerelease: true,
     },
   ],
-  plugins: ["@semantic-release/changelog", "@semantic-release/git"],
+  debug: true,
+  tagFormat: "v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/git",
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/exec",
+      {
+      "verifyReleaseCmd": "echo \"VERSION=${nextRelease.version}\" > RELEASE.env"
+      }
+    ],
+  ]
 };
