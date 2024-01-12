63d9a9028d57b5ba270f6eac86a27e2d05df1e64
t18n <me@t18n.com>
2024-01-12 23:27:22 +0100

    fix: github action

--- release.config.cjs
+++ release.config.cjs
@@ -17,14 +17,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    // "@semantic-release/changelog",
+    "@semantic-release/changelog",
     "@semantic-release/git",
     // "@semantic-release/github",
     // "@semantic-release/npm",
     [
       "@semantic-release/exec",
       {
-        "prepareCmd": "set-version ${nextRelease.version}",
+        "prepareCmd": "npm version ${nextRelease.version}",
         "verifyReleaseCmd": "echo \"VERSION=${nextRelease.version}\" > RELEASE.env"
       }
     ],
